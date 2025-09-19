907529501962d450d9566ac9e5212f7a37747196
Akiiila11 <akilavenkatraj2001@gmail.com>
2025-09-19 15:16:29 +0530

    Updated

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
-const inputNum = document.getElementById("number")
-const btn = document.getElementById("button")
+const btn = document.getElementById("button");
 
 btn.addEventListener("click" , () => {
-    const value = inputNum.value
-    inputNum.value = value
+    const inputNum = document.getElementById("number").value;
 
-    const api = fetch(' https://phonevalidation.abstractapi.com/v1/?api_key=05110cf63d354bf5be4ca3d5e008e8d9&phone=${inputNum} ')
-    
+    // const value = inputNum.value
+    // inputNum.value = value
+
+    // const api = fetch(` https://phonevalidation.abstractapi.com/v1/?api_key=05110cf63d354bf5be4ca3d5e008e8d9&phone=${inputNum} `)
+    const api = fetch(
+    `https://phonevalidation.abstractapi.com/v1/?api_key=05110cf63d354bf5be4ca3d5e008e8d9&phone=${inputNum}`
+  );
     // When the response comes back, turn it into JSON
     api.then((data) => data.json())
     // When the JSON is ready, show it or put it on the page
     .then((ele) => {
         
     // the actual phone number
-    const phone = document.getElementById("phone")
-    phone.innerText = ele.phone
+    const phone = document.getElementById("phone");
+    phone.innerText = ele.phone;
 
     // true/false (is it real?)
-    const valid = document.getElementById("valid")
-    valid.innerText = ele.valid
+    const valid = document.getElementById("valid");
+    valid.innerText = ele.valid;
 
     // Internatinal format
-    const international = document.getElementById("international-format")
-    international.innerText = ele.format.international
+    const international = document.getElementById("internationl-format");
+    international.innerText = ele.format.international;
 
     // Local format
-    const local = document.getElementById("local-format")
-    local.innerText = ele.format.local
+    const local = document.getElementById("local-format");
+    local.innerText = ele.format.local;
 
     // country code 
-    const code = document.getElementById("country-code")
-    code.innerText = ele.country.name
+    const code = document.getElementById("country-code");
+    code.innerText = ele.country.code;
 
+    // country name 
+    const countryName = document.getElementById("country-name");
+    countryName.innerText = ele.country.name
     // country prefix
-    const prefix = document.getElementById("country-prefix")
-    prefix.innerText = ele.country.prefix 
+    const prefix = document.getElementById("country-prefix");
+    prefix.innerText = ele.country.prefix ;
 
     // country location
-    const location = document.getElementById("location")
-    location.innerText = ele.location
+    const location = document.getElementById("location");
+    location.innerText = ele.location;
 
     // Type
-    const type = document.getElementById("type")
-    type.innerText = ele.type
+    const type = document.getElementById("type");
+    type.innerText = ele.type;
 
     // carrier
-    const carrier = document.getElementById("network")
-    carrier.innerText = ele.carrier
+    const carrier = document.getElementById("network-carrier");
+    carrier.innerText = ele.carrier;
 
-
-    })
-})
+    });
+});
